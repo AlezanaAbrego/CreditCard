645ddbd540c9bc4dd6021d065b03c958d3e62183
Alexia Abrego <alarabregode@ittepic.edu.mx>
2018-05-19 18:01:04 -0500

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Stripe } from '@ionic-native/stripe';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  card: any = {
+    number: '',
+    expMonth: '',
+    expYear: '',
+    cvc: ''
+  };
+
+  
+
+  constructor(public navCtrl: NavController, public stripe: Stripe) {
+    this.stripe.setPublishableKey('pk_test_006zCNEHDQQF6cqecQft5RfN');
+  }
+
+  ionViewDidLoad(){
+    this.stripe.setPublishableKey('pk_test_006zCNEHDQQF6cqecQft5RfN');
+  }
+
+
+  send(){
+    console.log(this.card);
+    this.stripe.createCardToken(this.card)
+    .then(token => console.log(token.id))
+    .catch(error => console.error(error));
+  }
+}
+
+
+
+
